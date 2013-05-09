a2d4d6fcdf38e61eaeeb630aa02b16ad02eb0b30
satoshi <satoshi.tuat@gmail.com>
2013-05-10 00:18:44 +0900

    Add Json

--- Twitty/ViewController.m
+++ Twitty/ViewController.m
@@ -7,11 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "SBJson.h"
 
 @interface ViewController ()
+@property ( nonatomic) IBOutlet UITextField *usernameTextFiel;
+@property ( nonatomic) IBOutlet UITextField *passwordTextField;
 
 @end
-
 @implementation ViewController
 
 - (void)viewDidLoad
@@ -24,6 +26,23 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+-(IBAction)login:(id)sender{
+    NSLog(@"%@",_usernameTextFiel.text);
+    NSLog(@"%@",_passwordTextField.text);
+    
+    NSURL *aUrl = [NSURL URLWithString:@"http://www.apple.com/"];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:aUrl
+                                                           cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                       timeoutInterval:60.0];
+    
+    NSURLConnection *connection= [[NSURLConnection alloc] initWithRequest:request
+                                                                 delegate:self];
+    
+    [request setHTTPMethod:@"POST"];
+    NSString *postString = @"company=Locassa&quality=AWESOME!";
+    [request setHTTPBody:[postString dataUsingEncoding:NSUTF8StringEncoding]];
+    
 }
 
 @end
